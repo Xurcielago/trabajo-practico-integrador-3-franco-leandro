7d44d38bae71cc69091fe2d8bf662817ab15a794
Xurcielago <leandrosian@hotmail.com>
2025-11-27 21:29:10 -0300

    Estructura del proyecto separada en carpetas (frontend y backend), desarrollado hook userForm

---
+++ frontend/src/assets/hooks/userForm.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+export const useForm = (initialForm = {}) => {
+  
+    const [formState, setFormState] = useState(initialForm);
+
+    const onInputChange = ({ target }) => {
+        const { name, value, type, checked } = target;
+
+        setFormState({
+            ...formState,
+             [name]: type === 'checkbox' ? checked : value
+        });
+    };
+
+    const onResetForm = () => {
+        setFormState(initialForm);
+    };
+
+    return {
+        ...formState,
+        formState,    
+        onInputChange,
+        onResetForm,
+    };
+};
